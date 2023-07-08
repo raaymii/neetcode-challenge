abbbd5c4ed89d0de2ee896c8f4bb2f3e856e4034
Osamagioghomwenwi Oghagbon <helloraaymii@gmail.com>
2023-07-08 12:14:51 +0100

    c001

--- containsduplicate.js
+++ containsduplicate.js
@@ -1,12 +1,15 @@
 //Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-function containsDuplicate(nums) {
-  const numbers = new Set();
-  for (const num of nums) {
-    if (numbers.has(num)) return true;
-    else numbers.add(num);
-  }
-  return false;
-}
+// function containsDuplicate(nums) {
+//   const numbers = new Set();
+//   for (const num of nums) {
+//     if (numbers.has(num)) return true;
+//     else numbers.add(num);
+//   }
+//   return false;
+// }
+
+const containsDuplicate = (nums) => new Set(nums).size !== nums.length;
+
 const nums1 = [1, 4, 5, 5, 7, 8, 0];
 console.log(containsDuplicate(nums1));
 
